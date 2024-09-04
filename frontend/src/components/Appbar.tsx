2b971e12aa66c8bc2435283de9e4263b70a3148c
Giridhar <giridharpatel143@gmail.com>
2024-09-04 20:50:02 +0530

    Add Logo

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,8 +4,9 @@ const Appbar = () => {
 
   return (
     <>
-      <div className="w-full h-20 rounded-sm bg-slate-100">
-
+      <div className="w-full h-20 shadow-xl rounded-2xl">
+        {/* <p className="bg-gradient-to-tr from-pink-500 via-purple-500 to-red-500">Devopos</p> */}
+        <p className="bg-gradient-to-tr from-pink-500 via-purple-500 to-red-500 text-5xl font-bold text-transparent bg-clip-text inline-block pt-3 ps-4">Devopos</p>
       </div>
     </>
   )
