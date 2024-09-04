e23f665b38a6acc29922ce0ec4b9d8b0c21cdd2c
Giridhar <giridharpatel143@gmail.com>
2024-09-04 20:06:45 +0530

    Add nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
-
+import Appbar from "./components/Appbar";
 
 function App() {
-
   return (
     <>
-
-      <div className='flex justify-center items-center w-full min-h-screen'>
-        <div className='rounded-xl shadow-xl bg-gradient-to-tr from-pink-600 via-purple-600 to-red-500 w-1/2 h-1/3 p-5'>
-          <h2 className='text-2xl font-semibold text-center text-white'>Let's Devops</h2>
+      <Appbar />
+      <div className="flex justify-center items-center w-full min-h-screen">
+          <div className="rounded-xl shadow-xl bg-gradient-to-tr from-pink-600 via-purple-600 to-red-500 w-1/2 h-96  p-5">
+            <h2 className="text-2xl font-semibold text-center text-white">
+              Let's Devops
+            </h2>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
